14233778ec98d325cf7f475a62eb32efe634ca76
naoki kodama <naoki.koda@gmail.com>
2015-01-04 07:21:35 +0900

    Initial Commit

---
+++ iKota/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iKota
+//
+//  Created by Naoki KODAMA on 2015/01/04.
+//  Copyright (c) 2015年 Naoki KODAMA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
