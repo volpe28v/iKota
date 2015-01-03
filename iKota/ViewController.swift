8ba275f6cd8e97670f3fb22ba7a29b28b64cc029
naoki kodama <naoki.koda@gmail.com>
2015-01-04 07:55:16 +0900

    プレーヤーの初期化処理を追加

--- iKota/ViewController.swift
+++ iKota/ViewController.swift
@@ -7,12 +7,51 @@
 //
 
 import UIKit
+import MediaPlayer
 
 class ViewController: UITableViewController {
+    var player : MPMusicPlayerController!
+    var query : MPMediaQuery!
+    var ncenter : NSNotificationCenter!
+    var tuneCollection : TuneCollection!
+    var relateItems : Array<AnyObject>!
+    var currentTuningBase : String!
+    var playingRelateIndex : Int!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        self.currentTuningBase = ""
+        
+        // 曲情報を取得
+        self.tuneCollection = TuneCollection()
+        
+        // 押尾コータローの全曲を取得して登録
+        self.player = MPMusicPlayerController.iPodMusicPlayer()
+        self.query = MPMediaQuery.artistsQuery()
+        var pred : MPMediaPropertyPredicate!
+        pred = MPMediaPropertyPredicate(value:"押尾コータロー", forProperty:MPMediaItemPropertyArtist)
+        self.query.addFilterPredicate(pred)
+        
+        // 再生変更通知を登録
+        self.ncenter = NSNotificationCenter.defaultCenter()
+        self.ncenter.addObserver(
+            self,
+            selector: "handle_NowPlayingItemChanged",   // 再生曲変更
+            name: MPMusicPlayerControllerNowPlayingItemDidChangeNotification,
+            object: self.player
+        )
+        self.ncenter.addObserver(
+            self,
+            selector: "handle_PlaybackStateDidChanged", // 再生状態変更
+            name: MPMusicPlayerControllerPlaybackStateDidChangeNotification,
+            object: self.player
+        )
+        
+        self.player.beginGeneratingPlaybackNotifications()
+        //self.setCurrentPlayOrStopButtonLabel()
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,5 +68,16 @@ class ViewController: UITableViewController {
         cell.textLabel?.text = "tune"
         return cell
     }
+    
+    
+    // 再生曲が変わったら呼ばれるハンドラ
+    func handle_NowPlayingItemChanged(){
+
+    }
+    
+    // 再生状態が変わったら呼ばれるハンドラ
+    func handle_PlaybackStateDidChanged(){
+    }
+
 }
 
