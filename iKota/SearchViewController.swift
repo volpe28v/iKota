a1be68d2bb6fefe3187c7a7ead26f8045e559609
naoki kodama <naoki.koda@gmail.com>
2015-01-27 00:00:15 +0900

    Search画面でTuning のテーブルを作成中
    テーブルの表示は出来たので、次はTuneCollector に Tuning 一覧を作成する

--- iKota/SearchViewController.swift
+++ iKota/SearchViewController.swift
@@ -10,10 +10,19 @@ import UIKit
 import MediaPlayer
 import AVFoundation
 
+enum SearchMode {
+    case Album
+    case Tuning
+    case History
+    case Count
+}
+
 class SearchViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     var tuneCollection : TuneCollection! = TuneCollection.sharedInstance
 
+    var searchMode : SearchMode = .Album
+    
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
@@ -29,10 +38,43 @@ class SearchViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return tuneCollection.activeAlbums.count
+        
+        switch self.searchMode
+        {
+        case .Album:
+            return tuneCollection.activeAlbums.count
+        case .Tuning:
+            return 2;
+        case .History:
+            break;
+        case .Count:
+            break;
+        default:
+            break;
+        }
+
+        return 0 // 通らないはず
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        switch self.searchMode
+        {
+        case .Album:
+            return getAlbumCell(tableView, cellForRowAtIndexPath: indexPath)
+        case .Tuning:
+            return getTuningCell(tableView, cellForRowAtIndexPath: indexPath)
+        case .History:
+            break;
+        case .Count:
+            break;
+        default:
+            break;
+        }
+        
+        return getAlbumCell(tableView, cellForRowAtIndexPath: indexPath)
+    }
+    
+    func getAlbumCell(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cellTmp = tableView.dequeueReusableCellWithIdentifier("album") as? UITableViewCell
         
         if cellTmp == nil {
@@ -50,12 +92,48 @@ class SearchViewController: UIViewController, UITableViewDataSource, UITableView
         
         var title = cell.viewWithTag(2) as UILabel
         title.text = tune.album
-
+        
+        return cell
+    }
+    
+    func getTuningCell(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cellTmp = tableView.dequeueReusableCellWithIdentifier("tuning") as? UITableViewCell
+        
+        if cellTmp == nil {
+            cellTmp = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "tuning")
+        }
+        
+        let cell = cellTmp!
+        
+        // Cellに部品を配置
+//        let tune : Tune = self.tuneCollection.activeAlbums[indexPath.row]
+        
+        var title = cell.viewWithTag(1) as UILabel
+        title.text = "DADGAD"
+        
         return cell
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         performSegueWithIdentifier("playlist", sender: self.tuneCollection.activeAlbums[indexPath.row])
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        switch self.searchMode
+        {
+        case .Album:
+            return 69
+        case .Tuning:
+            return 41
+        case .History:
+            break;
+        case .Count:
+            break;
+        default:
+            break;
+        }
+        
+        return 0
     }
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
@@ -65,4 +143,23 @@ class SearchViewController: UIViewController, UITableViewDataSource, UITableView
             playListController.displayAlbum = tune.album
         }
     }
+    
+    @IBAction func onClickSearchMode(sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex
+        {
+        case 0: // Album
+            self.searchMode = .Album
+        case 1: // Tuning
+            self.searchMode = .Tuning
+        case 2: // History
+            self.searchMode = .History
+        case 3: // Count
+            self.searchMode = .Count
+        default:
+            break;
+        }
+        self.tableView.reloadData()
+        
+    }
+    
 }
